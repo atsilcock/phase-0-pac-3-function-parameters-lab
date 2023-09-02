bdadb96662fe222eec73f41e0d058ec19b66a37e
Alin <at.silcock@gmail.com>
2023-09-02 16:46:23 -0600

    lab 1

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name) { 
+    return `Hi, my name is ${name}.`
+  }
+  function introductionWithLanguage(name1,name2) {
+    return `Hi, my name is ${name1} and I am learning to program in ${name2}.`
+  }
+  function introductionWithLanguageOptional (name,language="JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  } 
